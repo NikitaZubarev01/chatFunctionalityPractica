26f308f49be71c6a44f846b4ca6750dfc760346b
NikitaZubarev01 <90709331+NikitaZubarev01@users.noreply.github.com>
2022-04-04 18:50:06 +0700

    04.04.22 (2)

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -7,30 +7,20 @@ import { gStyle } from '../styles/style';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function SearchScreen({navigation}){
-    const [value, setValue] = React.useState("");
+    const [search, setSearch] = React.useState('');
     return(
         <View>
-            <View style={{ flexDirection:'row', paddingHorizontal: 10, }}>
-                <Button
-                 buttonStyle={{ width: 50,height: 50, backgroundColor: 'white', }}
-                 icon={
-                    <Ionicons 
-                    name="arrow-back" 
-                    size={30} 
-                    color="black" />
-                  }
-                 onPress={() => navigation.goBack()}
-                />
+            <View style={{}}>
                 <SearchBar
                     platform="android"
-                   // onChangeText={newVal => setValue(newVal)}
-                    onClearText={() => console.log(onClearText())}
                     placeholder="Поиск"
                     placeholderTextColor="black"
-                    cancelButtonTitle="Cancel"
-                    cancelButtonProps={{}}
-                    onCancel={() => console.log(onCancel())}
-                    value={value}
+                    onChangeText={search => setSearch(search)}
+                    onClearText={() => console.log(onClearText())}
+                   // cancelButtonTitle="Cancel"
+                    //cancelButtonProps={{}}
+                    onCancel={() => navigation.goBack()}
+                    value={search}
                 />
             </View>
         </View>
